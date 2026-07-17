d5a07f228380b472aabc0593e0fe3ed9bef65c70
gursimar singh <gursimar070@gmail.com>
2026-07-17 03:38:10 +0000

    Jul 17, 2026, 9:08 AM

--- practice/inheritance.py
+++ practice/inheritance.py
@@ -1,19 +1,24 @@
-class Student:
+class Checkerr:
+    def __init__(self,name):
+        self.name = name
+    def check_name(self):
+        if (len(self.name)>0):
+            return f"name={self.name} is valid"
+        else:
+            return f"invalid name"
+class Student(Checkerr):
     def __init__(self,name,age):
-        self.name=name
+        super().__init__(name)
+        # self.name=name
         self.age=age
-    def __str__(self):
+    def display_data(self):
         return f"name:{self.name} age:{self.age}"
 def main():
     name=input("name")
     age=input("age")
     student=Student(name,age)
-    print(student)
-
-
-
-
-
+    print(student.display_data)
+    print(student.check_name)
 if __name__=="__main__":
     main()
     
