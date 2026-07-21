5cd7631f22e70e9a05e3fc248ff10d79b5c29f0f
gursimar-2007 <gursimar070@gmail.com>
2026-07-21 13:14:37 +0530

    news

--- det.py
+++ det.py
@@ -1,3 +1,15 @@
+n1=int(input("please enter the number"))
+n2=int(input("please enter the number"))
+n3=int(input("please enter the number"))
+n4=int(input("please enter the number"))
+n5=int(input("please enter the number"))
+n6=int(input("please enter the number"))
+n7=int(input("please enter the number"))
+n8=int(input("please enter the number"))
+n9=int(input("please enter the number"))
 det=[
-    [1]
+    1:{"a11":n1,"a12":n2,"a13":n3},
+    2:{"a21":n4,"a22":n5,"a23":n6},
+    3:{"a31":n7,"a32":n8,"a33":n9}
 ]
+for items in det
