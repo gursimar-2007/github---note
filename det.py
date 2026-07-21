70dea495ab8e17fbaa2d5a529e1718d5f4c50f03
gursimar-2007 <gursimar070@gmail.com>
2026-07-21 08:03:57 +0000

    Jul 21, 2026, 1:33 PM

--- det.py
+++ det.py
@@ -16,5 +16,5 @@ print("your matrix is this thingy")
 for k,v in det.items():
     print(f"{v["frow"]} {v["srow"]} {v["trow"]}")
 print("YOUR DETERMINANT IS")
-print(f"{(n1*((n5*n9)-(n8*n6))) - (n2*((n4*n9)-(n7-n6))) +(n3*((n4*n8)-(n7*n5)))}")
+print(f"{(n1*((n5*n9)-(n8*n6))) - (n2*((n4*n9)-(n7*n6))) +(n3*((n4*n8)-(n7*n5)))}")
 
