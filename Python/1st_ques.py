08f50a5acea4c965435f22b231429cb0d0166a68
gursimar singh <gursimar070@gmail.com>
2026-07-22 14:32:23 +0000

    Jul 22, 2026, 8:02 PM

--- Python/1st_ques.py
+++ Python/1st_ques.py
@@ -1,13 +1,13 @@
 # try:
-#     print("please enter the marks of your subjects")
-#     sub1=int(input("enter the number of 1st subject"))
-#     sub2=int(input("enter the number of 2nd subject"))
-#     sub3=int(input("enter the number of 3rd subject"))
-#     sub4=int(input("enter the number of 4th subject"))
-#     sub5=int(input("enter the number of 5th subject"))
-#     total=sub1+sub2+sub3+sub4+sub5
+#     subjects=[]
+#     for i in range(1,6):
+#         marks=int(input("enter your marks please"))
+#         if marks<0 or marks>100:
+#             raise ValueError("invalid")
+#         subjects.append(marks)
+#     total=sum(subjects)
 #     percentage=(total/500)*100
-#     average=total/5
+#     average=total/len(subjects)
 #     print(f"{total},{average},{percentage}")
 #     if(percentage>=90 and percentage<=100):
 #         print("your grade is A please proceed")
@@ -21,20 +21,23 @@
 #         print("your grade is F please proceed with caution")
 # except ValueError:
 #     print("unethical")
+    
 
 # ======================================================================2nd question=====================================================
-# acc_bal=10000
-# withdrawal=acc_bal
-# if (withdrawal>10000 and withdrawal<0):
-#     print("invalid input please reverify")
-# elif(acc_bal>0):
-#     while acc_bal>0:
-#         withdrawal=int(input("enter withdrawal amount please"))
-#         acc_bal-=withdrawal
-#         print(acc_bal)
+acc_bal=10000
+withdrawal=acc_bal
+while acc_bal>0:
+        withdrawal=int(input("enter withdrawal amount please"))
+        acc_bal-=withdrawal
+        print(acc_bal)
+if (withdrawal>10000 or withdrawal<0):
+    print("invalid input please reverify")
        
-# elif(withdrawal>acc_bal or withdrawal==0):
-#     print("SBI ON LUNCh BREAK")
+elif(withdrawal>acc_bal or withdrawal==0):
+    print("SBI ON LUNCh BREAK")
+else:
+     acc_bal-=withdrawal
+     print(f"TRANSACTION SUCCESSFUL YOU ACC BALANCE IS NOW {acc_bal}")
     
     # ===========================================3rd question===============================================
 
@@ -68,24 +71,24 @@
 # print("not efined")
 # =================================================5th questions====================================
 
-import random
+# import random
 
-number = random.randint(1, 100)
+# number = random.randint(1, 100)
 
 
-for i in range(1, 7):
-    n = int(input(f"Attempt {i}/6 - Enter your guess (1-100): "))
-    if number == n:
-        print("Thank you, done!")
-        break  
-    elif number < n <= number + 10:
-        print("Too high, but close!")
-    elif n > number + 10:
-        print("too high!")
+# for i in range(1, 7):
+#     n = int(input(f"Attempt {i}/6 - Enter your guess (1-100): "))
+#     if number == n:
+#         print("Thank you, done!")
+#         break  
+#     elif number < n <= number + 10:
+#         print("Too high, but close!")
+#     elif n > number + 10:
+#         print("too high!")
         
         
     
-    elif n < number - 10:
-        print("too low")
+#     elif n < number - 10:
+#         print("too low")
 
 
