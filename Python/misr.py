916d52d739eb2a2878467be94ebf3aa989eed9de
gursimar singh <gursimar070@gmail.com>
2026-07-23 14:55:48 +0000

    Jul 23, 2026, 8:25 PM

--- Python/misr.py
+++ Python/misr.py
@@ -1,15 +1,14 @@
-list={
-    1:[1,2,3,4],
-    2:[5,6,7,8],
-    3:[9,10,11,12],
-    4:[13,14,15,16]
-}
-for k,v in list.items():
-    print(k)
-# for items in list.pop(0):
-#     print(items,end="")
-# for row in list:
-#     popped_integer = row.pop(3)
-#     print(popped_integer,end="")
-# for coloumn in list.reverse():
-#     print(coloumn)
+list=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+for items in list.pop(0):
+    print(items,end="")
+for sublist in list:
+    print(sublist[-1],end="")
+# for reverser in list:
+    # print(reverser)
+print(list[::-1])
+
