17ab729becc61b280799155e14eae087f2c1c3d2
gursimar singh <gursimar070@gmail.com>
2026-07-25 04:50:22 +0000

    Jul 25, 2026, 10:20 AM

--- practice/nitin.py
+++ practice/nitin.py
@@ -1,36 +1,52 @@
+# # while True:
+# #     if name=="nitin":
+# #         with open("freindsname.txt","w") as file:
+# #             pass
+# #     else:
+# #         with open ("freindsname.txt","a") as file:
+# #             file.write(f"{name}\n")
 # while True:
-#     if name=="nitin":
-#         with open("freindsname.txt","w") as file:
-#             pass
-#     else:
-#         with open ("freindsname.txt","a") as file:
+#     name=input("enter the name").lower()
+#     with open ("freindsname.txt","a") as file:
 #             file.write(f"{name}\n")
+
+#     while True:
+#         ask =input("\nDo you want to enter again? (Y/N): ").strip().lower()
+#         if ask == "y":
+#             print("new")
+#             with open ("freindsname.txt","a") as file:
+#                     file.write(f"{name}\n")
+#             break  
+#         elif ask == "n":
+#             print("Thanks Goodbye.")
+#             False
+                    
+#         else:
+#             print("Please enter 'Y' for Yes or 'N' for No.")
+#     print("--- Friends List ---")
+#     with open("freindsname.txt", "r") as file:
+#         lines = file.readlines()
+#         print(lines.strip())
+        # for line in lines:
+
 while True:
-    name=input("enter the name").lower()
-    with open ("freindsname.txt","a") as file:
-            file.write(f"{name}\n")
+    name = input("Enter the name: ").strip().lower()
+
+    with open("freindsname.txt", "a") as file:
+        file.write(f"{name}\n")
 
     while True:
-        ask =input("\nDo you want to enter again? (Y/N): ").strip().lower()
-        if ask == "y":
-            print("new")
-            with open ("freindsname.txt","a") as file:
-                    file.write(f"{name}\n")
-            break  
-        elif ask == "n":
-            print("Thanks Goodbye.")
+        ask = input("Do you want to enter again? (Y/N): ").strip().lower()
+        if ask in ("y", "n"):
             break
-                    
-        else:
-            print("Please enter 'Y' for Yes or 'N' for No.")
-    print("--- Friends List ---")
-    with open("freindsname.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            print(line.strip())
+        print("Please enter 'Y' for Yes or 'N' for No.")
 
-# while
-# with open a
-    #input
-    # write
-    # if coti
+    if ask == "n":
+        print("Thanks! Goodbye.\n")
+        break
+
+print("--- Friends List ---")
+with open("freindsname.txt", "r") as file:
+    lines = file.readlines()
+    print(lines)
+    # for line in lines:
