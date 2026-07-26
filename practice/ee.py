cc775ef45de9f81349749c5117d370b039853d8f
gursimar singh <gursimar070@gmail.com>
2026-07-26 22:44:35 +0530

    Update ee.py

--- practice/ee.py
+++ practice/ee.py
@@ -1,7 +1,13 @@
-n=(input("enter your character"))
-z=0
-for i in range(len(n)):
-    if n[i]=="a" or n[i]=="i" or n[i]=="e" or n[i]=="o" or n[i]=="u":
-        z+=1
-        print(z)
-    
+# n=(input("enter your character"))
+# z=0
+# for i in range(len(n)):
+#     if n[i]=="a" or n[i]=="i" or n[i]=="e" or n[i]=="o" or n[i]=="u":
+#         z+=1
+#         print(z)
+# n=input("enter your string")
+# print(n[::-1])
+# list=[1,2,3,4,5,6]
+# sum=0
+# for i in range(len(list)):
+#     sum=sum+list[i]
+# print(sum)
