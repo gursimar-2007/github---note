c179f4ccdc7dc36186f410aa542b0050eb3a632e
gursimar singh <gursimar070@gmail.com>
2026-07-29 16:53:00 +0000

    Jul 29, 2026, 10:23 PM

--- Python/=.py
+++ Python/=.py
@@ -1,1 +1,3 @@
-from openai import OpenAi
+#let a round table with n people and every 3rd person gets eliminated
+n=int(input("enter the number of people on the table"))
+print(f"the length of people on table are{n}")
