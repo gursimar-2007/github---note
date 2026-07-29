be3a266d4305d234bc4fe53f504f7349e3f3630f
gursimar singh <gursimar070@gmail.com>
2026-07-29 22:58:17 +0530

    Update =.py

--- Python/=.py
+++ Python/=.py
@@ -1,3 +1,9 @@
 #let a round table with n people and every 3rd person gets eliminated
 n=int(input("enter the number of people on the table"))
+skipper=0
 print(f"the length of people on table are{n}")
+for i in range(1,n+1):
+    if i%3==0:
+        continue
+    print(i)
+        
