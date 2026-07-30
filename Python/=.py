713b60b50d45d749bfd73198b4d02841e993f5ba
gursimar-2007 <gursimar070@gmail.com>
2026-07-30 17:19:50 +0000

    Jul 30, 2026, 10:49 PM

--- Python/=.py
+++ Python/=.py
@@ -1,11 +1,12 @@
 lists=[1,2,3,4,5,6,7,8,9,10]
-
+current_person=1
 count=1
 for list in lists:
     count+=1
+    current_person+=1
     if count==3:
         count==0
-        lists.remove(3)
+        lists.remove(current_person)
                 
 print(lists)
         
