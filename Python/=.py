7f962b100dcb5a09fc0685bddc11ba6b46d6fa34
gursimar-2007 <gursimar070@gmail.com>
2026-07-30 17:14:34 +0000

    Jul 30, 2026, 10:44 PM

--- Python/=.py
+++ Python/=.py
@@ -1,15 +1,11 @@
-#let a round table with n people and every 3rd person gets eliminated
+lists=[1,2,3,4,5,6,7,8,9,10]
 
-
-# n=int(input("enter the number of people on the table"))
-lists=[1,2,3,4,5]
-skipper=0
-# print(f"the length of people on table are{n}")
 count=1
 for list in lists:
-    count+=3
+    count+=1
     if count==3:
+        count==0
         lists.remove(3)
                 
-print(list)
+print(lists)
         
