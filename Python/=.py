01c3a39a554a3da31c917d6bb065b4c4472860d7
gursimar-2007 <gursimar070@gmail.com>
2026-07-30 17:27:56 +0000

    Jul 30, 2026, 10:57 PM

--- Python/=.py
+++ Python/=.py
@@ -1,10 +1,12 @@
 lists=[1,2,3,4,5,6,7,8,9,10]
 count=1
+for index in range(0,len(lists)):
+    pass
 while len(lists)>1:
     count+=1
     if count==3:
         count=0
         lists.remove(count)
-                
+        (index + 1) % len(lists)
 print(lists)
         
