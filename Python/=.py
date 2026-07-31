c6cb78d4d6fdfa4353669289d40c46a4b5e8c8ce
gursimar-2007 <gursimar070@gmail.com>
2026-07-31 07:11:57 +0000

    Jul 31, 2026, 12:41 PM

--- Python/=.py
+++ Python/=.py
@@ -1,15 +1,22 @@
-people=[1,2,3,4,5,6,7,8,9]
-index=0
-count=1
-while len(people)>1:
-    # index+=1
-    count+=1
-    if count==3:
+people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+index = 0
+count = 0
+
+while len(people) > 1:
+    count += 1
+
+    if count == 3:
+        print(f"{people[index]}")
         people.pop(index)
-        count=0
+        count = 0
+
+        if len(people) > 0:
+            index = index % len(people)
     else:
-         index = (index + 1) % len(people)
-print(people[0])
+        index = (index + 1) % len(people)
+
+print("Winner:", people[0])
 #         people = [...]
 # index = ?
 # count = 0
