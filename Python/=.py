e0eab5beac5e6bfa90924db01f887369076c7b2d
gursimar-2007 <gursimar070@gmail.com>
2026-07-31 08:02:41 +0000

    Jul 31, 2026, 1:32 PM

--- Python/=.py
+++ Python/=.py
@@ -17,15 +17,3 @@ while len(people) > 1:
         index = (index + 1) % len(people)
 
 print("Winner:", people[0])
-#         people = [...]
-# index = ?
-# count = 0
-
-# while more than one person remains:
-#     count += 1
-
-#     if count == 3:
-#         remove the person at index
-#         reset count
-#     else:
-#         move index to the next person (wrap around if needed)
