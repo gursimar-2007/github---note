9464d8e2ffa2dc9309eca70f43280e78362a5193
gursimar-2007 <gursimar070@gmail.com>
2026-08-03 16:57:35 +0000

    Aug 3, 2026, 10:27 PM

--- Python/new.cpp
+++ Python/new.cpp
@@ -1,8 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    people=[1,2,3,4,5,6,7]
+    int people[5] = {1,2,3,4,5};
+    int index=0;
+    int count=0
+    while size(grades)>0{
+        count++;
+        if (count==3){
+            printf(people[index])
 
-    return 0
+
+        }
+    }
+
+    return 0;
 }
 
