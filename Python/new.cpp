1b23a282a7f4920cf041b7f591aa472ba74879a2
gursimar-2007 <gursimar070@gmail.com>
2026-08-27 15:37:21 +0000

    Aug 27, 2026, 9:07 PM

--- Python/new.cpp
+++ Python/new.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int people[5] = {1,2,3,4,5};
     int index=0;
-    int count=0
+    int count=0;
     while size(grades)>0{
         count++;
         if (count==3){
