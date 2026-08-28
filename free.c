112e250c4e439bd79d923348829b7a3211621315
gursimar-2007 <gursimar070@gmail.com>
2026-08-28 05:36:18 +0000

    Aug 28, 2026, 11:06 AM

--- free.c
+++ free.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 int main(){
-    
+    int a;
+    int b;
+    printf("enter first side");
+    scanf("%d",&a);
+    printf("enter second side");
+    scanf("%d",&b);
+    int per;
+    per=a*b;
+    printf("the permeter of rectangle is :%d",per);
+
 
     return 0;
 }
