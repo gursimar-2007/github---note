aa5060a002eee0512b2eb1ece7e8f11c70471eb7
gursimar-2007 <gursimar070@gmail.com>
2026-08-29 16:35:30 +0000

    Aug 29, 2026, 10:05 PM

--- divisible.c
+++ divisible.c
@@ -1,12 +1,20 @@
+// #include<stdio.h>
+// void main(){
+//     int a;
+//     printf("enter no");
+//     scanf("%d",&a);
+//     if(a%3==0){
+    //         printf("this number is divisible");
+    //     }else{
+        //         printf("the number is not divisible");
+        //     }
+        
+        // }
 #include<stdio.h>
 void main(){
-    int a;
-    printf("enter no");
-    scanf("%d",&a);
-    if(a%3==0){
-        printf("this number is divisible");
-    }else{
-        printf("the number is not divisible");
+    int i,j,count;
+    int arr[10]={1,2,3,4,5,6,7};
+    for(int i=0;i<=sizeof(arr);i++){
+        printf(arr[i]);
     }
-    
 }
