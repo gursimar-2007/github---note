831aca29716bd6d3d051eda23f9d42137c41a708
gursimar-2007 <gursimar070@gmail.com>
2026-09-03 15:52:50 +0000

    Sep 3, 2026, 9:22 PM

--- str.c
+++ str.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[]="FATEHSIR";
-    printf("THE length OF THIS STRING IS=%s\n",strlwr(str));
+    char str1[]="fateh";
+    char str2[]=" sir";
+    printf("the length of the characters is %d\n",strlen(str));
+    printf("the REVERSE of the characters is %s\n",strrev(str));
+    printf("THE LOWERCASE FORMAT OF THIS STRING IS=%s\n",strlwr(str));
+    printf("THE UPPERCASE FORMAT OF THIS STRING IS=%s\n",strupr(str));
+    strcpy(str2,str1);
+    printf("copied string is %s",str2);
+    strcat(str1,str2);
+    printf("concated string =%s",str1);
     return 0;
 }
